10975c16c09cd4ecd527dedcd00a13b22436d76a
juanpablocs <juanpablocs21@gmail.com>
2023-01-20 13:37:24 -0500

    fix: update readme and change version manifest

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Cryptocurrency Live
 
+<p align="center">
+  <a href="https://github.com/juanpablocs/cryptocurrency-live">
+    <img src="public/img/overview.png">
+  </a>
+</p>
+
 install dependencies
 
 ```
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -9,11 +9,15 @@ export const TopBar = () => {
   const windowSize = useSelector((state) => state.ui.windowSize);
   const windowSizes = ['1h', '1d', '7d'];
 
+  const handlerClickSetting = () => {
+    alert('wip');
+  };
+
   return (
     <>
       <div className='top--left'>
-        <span className='btn'><IoMdSettings /></span>
-        <span className='btn'> <FiCoffee /> <span style={{ marginLeft: 5, position: 'relative', bottom: -0.5 }}>Donate coffe</span></span>
+        <span className='btn' onClick={handlerClickSetting}><IoMdSettings /></span>
+        {/* <span className='btn'> <FiCoffee /> <span style={{ marginLeft: 5, position: 'relative', bottom: -0.5 }}>Donate coffe</span></span> */}
 
       </div>
       <ul>
